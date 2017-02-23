2c2e3e70385f5a4ef1a7111e262cae3f8d113029
al3xst <al3xst@users.noreply.github.com>
2017-02-23 16:50:10 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='knossos-nightly',
+    version='nighlty',
+
+    description='Test configuration for deployment to PyPi by Travis CI',
+    url='https://github.com/al3xst/knossos-nightly',
+)
